8a2cc9eb8924933390287ade42a8696e00e5b213
Frank Clarke <frankjmclarke@gmail.com>
2024-02-25 18:12:27 -0800

    Create roomData.js
    Restructure room data

---
+++ roomData.js
@@ -0,0 +1,74 @@
+const roomData = [
+    { 
+        roomString: "T-Junction", 
+        size: [2, 2], 
+        fillColor: "red", 
+        lineColor: "blue" 
+    },
+    { 
+        roomString: "Dead End", 
+        size: [2, 2], 
+        fillColor: "blue", 
+        lineColor: "green" 
+    },
+    { 
+        roomString: "Right Turn", 
+        size: [2, 2], 
+        fillColor: "green", 
+        lineColor: "yellow" 
+    },
+    { 
+        roomString: "Left Turn", 
+        size: [2, 2], 
+        fillColor: "yellow", 
+        lineColor: "black" 
+    },
+    { 
+        roomString: "Stairs Down", 
+        size: [2, 2], 
+        fillColor: "black", 
+        lineColor: "red" 
+    },
+    { 
+        roomString: "Stairs Out", 
+        size: [2, 2], 
+        fillColor: "red", 
+        lineColor: "blue" 
+    },
+    { 
+        roomString: "Corridor", 
+        size: [5, 2], 
+        fillColor: "blue", 
+        lineColor: "green" 
+    },
+    { 
+        roomString: "Normal", 
+        size: [5, 5], 
+        fillColor: "green", 
+        lineColor: "yellow" 
+    },
+    { 
+        roomString: "Hazard", 
+        size: [5, 5], 
+        fillColor: "yellow", 
+        lineColor: "black" 
+    },
+    { 
+        roomString: "Lair", 
+        size: [5, 10], 
+        fillColor: "black", 
+        lineColor: "red" 
+    },
+    { 
+        roomString: "Quest", 
+        size: [5, 10], 
+        fillColor: "red", 
+        lineColor: "green" 
+    },
+    { 
+        roomString: "Door", 
+        size: [2, 1], 
+        fillColor: "orange", 
+        lineColor: "white" 
+    }
+];
